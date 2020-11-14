2b191b021f1afdc5b80a03ac6e83830dc713e653
XuanRan0808 <3135419729@qq.com>
2020-11-14 14:08:11 +0800

    add fragment

--- app/src/main/java/com/XuanRan/BottomTest/MainActivity.java
+++ app/src/main/java/com/XuanRan/BottomTest/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.viewpager.widget.ViewPager;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
 import com.gauravk.bubblenavigation.BubbleNavigationLinearView;
 import com.gauravk.bubblenavigation.listener.BubbleNavigationChangeListener;
 import java.util.ArrayList;
@@ -19,14 +21,6 @@ public class MainActivity extends FragmentActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<ScreenSlidePageFragment> fragList = new ArrayList<>();
-        fragList.add(ScreenSlidePageFragment.newInstance("home", R.color.red_inactive));
-        fragList.add(ScreenSlidePageFragment.newInstance("search", R.color.blue_inactive));
-        fragList.add(ScreenSlidePageFragment.newInstance("likes", R.color.blue_grey_inactive));
-        fragList.add(ScreenSlidePageFragment.newInstance("notification", R.color.green_inactive));
-        fragList.add(ScreenSlidePageFragment.newInstance("profile", R.color.purple_inactive));
-
-        ScreenSlidePagerAdapter pagerAdapter = new ScreenSlidePagerAdapter(fragList, getSupportFragmentManager());
 
         final BubbleNavigationLinearView bubbleNavigationLinearView = findViewById(R.id.bottom_navigation_view_linear);
      //   bubbleNavigationLinearView.setTypeface(Typeface.createFromAsset(getAssets(), "rubik.ttf"));
@@ -37,28 +31,13 @@ public class MainActivity extends FragmentActivity
         bubbleNavigationLinearView.setBadgeValue(3, "2");
         bubbleNavigationLinearView.setBadgeValue(4, ""); //empty badge
 
-        final ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(pagerAdapter);
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int i, float v, int i1) {
-            }
-
-            @Override
-            public void onPageSelected(int i) {
-                bubbleNavigationLinearView.setCurrentActiveItem(i);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int i) {
-
-            }
-        });
+   
 
         bubbleNavigationLinearView.setNavigationChangeListener(new BubbleNavigationChangeListener() {
             @Override
             public void onNavigationChanged(View view, int position) {
-                viewPager.setCurrentItem(position, true);
+                Toast.makeText(MainActivity.this, "点击了底栏", Toast.LENGTH_SHORT).show();
+                //viewPager.setCurrentItem(position, true);
             }
         });
     }
